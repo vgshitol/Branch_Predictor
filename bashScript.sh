308c080cb26dd73fd6e003675d7910c33f28b584
Vishal Shitole <vgshitol@ncsu.edu>
2018-11-14 00:45:17 -0500

    Report Finalised

--- bashScript.sh
+++ bashScript.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+mkdir graph graph/graph1 graph/graph2
 
 make clean
 make
@@ -18,4 +19,27 @@ for i in 7 8 9 10 11 12
     echo "$variable4,$variable3, $variable2, $variable1" >> ./graph/graph1/graph1.csv
 
   echo "Looping ... number $i"
+done
+
+#Graph 2
+   echo 'M' , 'N', 'Misprediction Rate GCC' ,'Misprediction Rate JPEG', 'Misprediction Rate PERL' >> ./graph/graph2/graph2.csv
+for i in 7 8 9 10 11 12
+ do
+ for j in 2 3 4 5 6 7 8 9 10 11 12
+ do
+ if (($i >= $j)); then
+
+  ./sim gshare ${i} ${j} ./proj2-traces/gcc_trace.txt > ./graph/graph2/outputgcc_m${i}${j}.txt
+  ./sim gshare ${i} ${j} ./proj2-traces/jpeg_trace.txt > ./graph/graph2/outputjpeg_m${i}${j}.txt
+  ./sim gshare ${i} ${j} ./proj2-traces/perl_trace.txt > ./graph/graph2/outputperl_m${i}${j}.txt
+    variable1=$(grep 'misprediction rate:' ./graph/graph2/outputgcc_m${i}${j}.txt | awk '{print $3}');
+    variable2=$(grep 'misprediction rate:' ./graph/graph2/outputjpeg_m${i}${j}.txt | awk '{print $3}');
+    variable3=$(grep 'misprediction rate:' ./graph/graph2/outputperl_m${i}${j}.txt | awk '{print $3}');
+    variable4=${j};
+    variable5=${i};
+    echo "$variable5, $variable4, $variable3, $variable2, $variable1" >> ./graph/graph2/graph2.csv
+
+  echo "Looping ... number $i $j"
+fi
+ done
 done
